83bfd86d7d55f4fda0c9d683a63b0cfcb8fefe63
briannphan21 <61403034+briannphan21@users.noreply.github.com>
2021-02-16 23:31:40 -0500

    Update URLWarning.tsx

--- src/components/Header/URLWarning.tsx
+++ src/components/Header/URLWarning.tsx
@@ -8,7 +8,7 @@ import { isMobile } from 'react-device-detect'
 const PhishAlert = styled.div<{ isActive: any }>`
   width: 100%;
   padding: 6px 6px;
-  background-color: ${({ theme }) => theme.blue1};
+  background-color: #cc9933;
   color: white;
   font-size: 11px;
   justify-content: space-between;
